f054811f3dbab14ef2539719b314322e8b164a64
Burinchai <96411085+Burinchai@users.noreply.github.com>
2023-12-15 13:37:22 +0700

    .commit

--- Home.py
+++ Home.py
@@ -89,4 +89,4 @@ if st.button("ทำนายผล"):
 else :
     st.button("ไม่ทำนายผล")
 
-st.video(https://youtu.be/RE_pN8ZUoNE?si=5hOB857RQdWZl_Y3, format="video/mp4", start_time=0)
+st.video("https://youtu.be/RE_pN8ZUoNE?si=5hOB857RQdWZl_Y3", format="video/mp4", start_time=0)
