e4864efebe2c7711a19167f524f0eb373a52ac55
Burinchai <96411085+Burinchai@users.noreply.github.com>
2023-12-15 14:04:58 +0700

    .commit

--- Home.py
+++ Home.py
@@ -43,7 +43,7 @@ else :
     st.button("Not show bar chart") 
 
 html_2 = """
-<style>*{ background-color:'red'}</style>
+<style>*{background-color:'red'}</style>
 <div style="background-color:#FFBF00;padding:15px;border-radius:15px 15px 15px 15px;border-style:'solid';border-color:black">
 <center><h5>การทำนายคลาสดอกไม้</h5></center>
 </div>
