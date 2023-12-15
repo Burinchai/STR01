4c55d800838fb12391544b16aba7decccc26f38a
Burinchai <96411085+Burinchai@users.noreply.github.com>
2023-12-15 14:35:12 +0700

    .commit

--- Home.py
+++ Home.py
@@ -1,4 +1,7 @@
 import streamlit as st
+from streamlit_lottie import st_lottie
+import json
+import requests
 
 st.header("Burinchai Sukon")
 
@@ -94,3 +97,24 @@ else :
 st.divider()
 
 st.video("https://youtu.be/N1fnq4MF3AE?si=T6TDBZSYzWgruUH7", format="video/mp4", start_time=0)
+
+st.divider()
+
+def load_lottieurl(url: str):
+    r = requests.get(url)
+    if r.status_code != 200:
+        return None
+    return r.json()
+
+st.header("การวิเคราะห์ความรู้สึกภาษาไทย")
+st.subheader("Kairung Hengpraprohm")
+
+col1, col2 = st.columns(2)
+with col1:
+    st.image('./pic/kairung.jpg')
+    lot3="https://lottie.host/7694433a-eb67-423d-a987-da760a88f4c2/ajfA7ELhKh.json"
+    lottie3 = load_lottieurl(lot3)
+    st_lottie(lottie3)
+with col2:
+    st.image('./pic/DS1.jpg')
+st.balloons()
