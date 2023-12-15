e956c401f5ad49eec1ab3b2c85a12732acfce758
Burinchai <96411085+Burinchai@users.noreply.github.com>
2023-12-15 13:27:30 +0700

    .commit

--- Home.py
+++ Home.py
@@ -11,7 +11,7 @@ col1, col2 = st.columns(2)
 #col1.write("This is column 1")
 #col2.write("This is column 2")
 with col1:
-    st.image('./pic/rung1.jfif')
+    st.image('./pic/elon.jpg')
 with col2:
     st.image('./pic/iris2.jpg')
 
